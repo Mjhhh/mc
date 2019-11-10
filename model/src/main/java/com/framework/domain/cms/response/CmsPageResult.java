de48ee526cfc60b2c0140d46a6ac1acf0267b554
Mjhhh <523651128@qq.com>
2019-11-10 15:00:15 +0800

    mooc实例

---
+++ model/src/main/java/com/framework/domain/cms/response/CmsPageResult.java
@@ -0,0 +1,18 @@
+package com.framework.domain.cms.response;
+
+import com.framework.domain.cms.CmsPage;
+import com.framework.model.response.ResponseResult;
+import com.framework.model.response.ResultCode;
+import lombok.Data;
+
+/**
+ * Created by mrt on 2018/3/31.
+ */
+@Data
+public class CmsPageResult extends ResponseResult {
+    CmsPage cmsPage;
+    public CmsPageResult(ResultCode resultCode,CmsPage cmsPage) {
+        super(resultCode);
+        this.cmsPage = cmsPage;
+    }
+}
